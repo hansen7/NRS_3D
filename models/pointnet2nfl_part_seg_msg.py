2713cb0f945c8335106c05c4c159b6834d09e30a
hansen7 <hc.wang96@gmail.com>
2020-04-26 04:44:56 +0100

    add pointnet++ nfl baselines

--- models/pointnet2nfl_part_seg_msg.py
+++ models/pointnet2nfl_part_seg_msg.py
@@ -1,7 +1,8 @@
 #  Copyright (c) 2020. Hanchen Wang, hw501@cam.ac.uk
 
+import os, sys, pdb, torch.nn as nn, torch.nn.functional as F
+sys.path.append(['./', '../'])
 from NFL import NFL
-import os, torch, torch.nn as nn, torch.nn.functional as F
 from data_utils.Dict2Object import Dict2Object
 from models.pointnet_util import PointNetSetAbstractionMsg, PointNetSetAbstraction, PointNetFeaturePropagation
 
