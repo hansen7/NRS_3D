beec7306450c4dae6757b610b9c327c7eed7b367
hansen7 <hc.wang96@gmail.com>
2020-05-28 03:12:09 +0100

    better organise FLOPs Calculator

--- data_utils/FLOPS_Calculator.py
+++ data_utils/FLOPS_Calculator.py
@@ -6,7 +6,7 @@ sys.path.append('../')
 sys.path.append('../models')
 from models.pointnetnfl_cls import get_model
 from models.pointnet2nfl_cls_ssg import get_model
-from models.pointnet2_cls_ssg import get_model
+# from models.pointnet2_cls_ssg import get_model
 from data_utils.Dict2Object import Dict2Object
 from models.dgcnn import DGCNN, DGCNN_NFL
 
@@ -14,8 +14,8 @@ from models.dgcnn import DGCNN, DGCNN_NFL
 if __name__ == "__main__":
 
 	''' === PointNet and PointNet++ === '''
-	model = get_model(num_class=40, nfl_cfg=Dict2Object(
-		'../nfl_config/pointnetnfl_cls.yaml')).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
+	model = get_model(k=40, nfl_cfg=Dict2Object(
+		'../nfl_config/pointnet_nfl_dh3_nmul1_nper32.yaml')).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
 	''' === DGCNN === '''
 	def parse_args():
@@ -25,7 +25,7 @@ if __name__ == "__main__":
 		parser.add_argument('--k', type=int, default=20, help='Num of nearest neighbors to use')
 		parser.add_argument('--emb_dims', type=int, default=1024, help='Dimension of Embeddings')
 		parser.add_argument('--num_point', type=int, default=1024, help='num of points of each object')
-		parser.add_argument('--nfl_cfg', type=str, default='../nfl_config/dgcnnnfl_cls.yaml', 
+		parser.add_argument('--nfl_cfg', type=str, default='../nfl_config/dgcnn_nfl_dh3_nmul1_nper128.yaml', 
 							help='config for NFL modules')
 		return parser.parse_args()
 	
@@ -34,8 +34,8 @@ if __name__ == "__main__":
 	model = DGCNN_NFL(args).to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 	
 	''' === Set up the Shape of Input === '''
-	# input = torch.randn(1, 6, 1024).cuda()
 	input = torch.randn(1, 3, 1024).cuda()
+	# input = torch.randn(1, 6, 1024).cuda()
 	macs, params = profile(model, inputs=(input, ))
 
 	print('\n======================')
